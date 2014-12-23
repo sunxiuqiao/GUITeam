054839c2ad7c0d35a5becccb52024b12c8599aa8
zhongzhao <1114753650@qq.com>
2014-12-23 17:14:23 +0800

    add shp file

--- ArcEngineTest/Form1.Designer.cs
+++ ArcEngineTest/Form1.Designer.cs
@@ -29,43 +29,34 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.axToolbarControl = new ESRI.ArcGIS.Controls.AxToolbarControl();
             this.axMapControl = new ESRI.ArcGIS.Controls.AxMapControl();
             this.axTOCControl = new ESRI.ArcGIS.Controls.AxTOCControl();
             this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl)).BeginInit();
+            this.AddData = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // axToolbarControl
-            // 
-            this.axToolbarControl.Dock = System.Windows.Forms.DockStyle.Top;
-            this.axToolbarControl.Location = new System.Drawing.Point(0, 49);
-            this.axToolbarControl.Name = "axToolbarControl";
-            this.axToolbarControl.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl.OcxState")));
-            this.axToolbarControl.Size = new System.Drawing.Size(469, 28);
-            this.axToolbarControl.TabIndex = 0;
             // 
             // axMapControl
             // 
             this.axMapControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.axMapControl.Location = new System.Drawing.Point(151, 77);
+            this.axMapControl.Location = new System.Drawing.Point(151, 49);
             this.axMapControl.Name = "axMapControl";
             this.axMapControl.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl.OcxState")));
-            this.axMapControl.Size = new System.Drawing.Size(318, 233);
+            this.axMapControl.Size = new System.Drawing.Size(490, 445);
             this.axMapControl.TabIndex = 1;
             // 
             // axTOCControl
             // 
             this.axTOCControl.Dock = System.Windows.Forms.DockStyle.Left;
-            this.axTOCControl.Location = new System.Drawing.Point(0, 77);
+            this.axTOCControl.Location = new System.Drawing.Point(0, 49);
             this.axTOCControl.Name = "axTOCControl";
             this.axTOCControl.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl.OcxState")));
-            this.axTOCControl.Size = new System.Drawing.Size(151, 233);
+            this.axTOCControl.Size = new System.Drawing.Size(151, 445);
             this.axTOCControl.TabIndex = 2;
             // 
             // axLicenseControl1
@@ -79,9 +70,11 @@
             // 
             // toolStrip1
             // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddData});
             this.toolStrip1.Location = new System.Drawing.Point(0, 24);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(469, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(641, 25);
             this.toolStrip1.TabIndex = 4;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -89,28 +82,37 @@
             // 
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(469, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(641, 24);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // AddData
+            // 
+            this.AddData.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.AddData.Image = ((System.Drawing.Image)(resources.GetObject("AddData.Image")));
+            this.AddData.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.AddData.Name = "AddData";
+            this.AddData.Size = new System.Drawing.Size(23, 22);
+            this.AddData.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 310);
+            this.ClientSize = new System.Drawing.Size(641, 494);
             this.Controls.Add(this.axLicenseControl1);
             this.Controls.Add(this.axMapControl);
             this.Controls.Add(this.axTOCControl);
-            this.Controls.Add(this.axToolbarControl);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,12 +120,12 @@
 
         #endregion
 
-        private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl;
         private ESRI.ArcGIS.Controls.AxMapControl axMapControl;
         private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl;
         private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripButton AddData;
     }
 }
 
--- ArcEngineTest/Form1.cs
+++ ArcEngineTest/Form1.cs
@@ -6,7 +6,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Carto;
+using System.IO;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesFile;
 namespace ArcEngineTest
 {
     public partial class Form1 : Form
@@ -15,5 +23,63 @@ namespace ArcEngineTest
         {
             InitializeComponent();
         }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            AddShpfile();
+        }
+        #region
+        /// <summary>
+        /// 添加SHP文件
+        /// </summary>
+        private void AddShpfile()    
+        {
+            //存储打开文件的全路径
+            string fullFilePath;
+            //设置OpenFileDialog的属性，使其能打开多种类型文件
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "shape文件(*.shp)|*.shp";
+
+            openFile.Title = "打开文件";
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    fullFilePath = openFile.FileName;
+                    //获得文件路径
+                    int index = fullFilePath.LastIndexOf("\\");
+                    string filePath = fullFilePath.Substring(0, index);
+                    //获得文件名称
+                    string fileNam = fullFilePath.Substring(index + 1);
+                    //加载shape文件
+                    if (openFile.FilterIndex == 1)
+                    {
+                        //打开工作空间工厂
+                        IWorkspaceFactory workspcFac = new ShapefileWorkspaceFactory();
+                        IFeatureWorkspace featureWorkspc;
+                        IFeatureLayer featureLay = new FeatureLayerClass();
+                        //打开路径
+                        featureWorkspc = workspcFac.OpenFromFile(filePath, 0) as IFeatureWorkspace;
+                        //打开类要素
+                        featureLay.FeatureClass = featureWorkspc.OpenFeatureClass(fileNam);
+                        //清空图层
+                        axMapControl.ClearLayers();
+                        //添加图层
+                        axMapControl.AddLayer(featureLay);
+                        axMapControl.Refresh();
+
+                    }
+                }
+            }
+
+
+            catch (Exception e)
+            {
+                MessageBox.Show("添加图层是失败" + e.ToString());
+
+            }
+        }
+        #endregion
     }
-}
+}      
+    

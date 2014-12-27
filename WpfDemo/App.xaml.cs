e9ad4cb421a9ff84f5def5f194e2a23437f89313
sxq <yanhejin@163.com>
2014-12-27 21:09:19 +0800

    add WPF

---
+++ WpfDemo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ESRI.ArcGIS.esriSystem;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.EngineOrDesktop);
+            AoInitialize aoi = new AoInitializeClass();
+            esriLicenseProductCode productCode = esriLicenseProductCode.esriLicenseProductCodeEngine;
+            if (aoi.IsProductCodeAvailable(productCode) == esriLicenseStatus.esriLicenseAvailable)
+            {
+                aoi.Initialize(productCode);
+            }  
+        }  
+    }
+}

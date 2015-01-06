3e5ef33e64914c517f49211f728daf4855a5a0e7
cgz <chenguanzhou123@163.com>
2015-01-06 22:07:32 +0800

    Add DockPanel Support to MVVM

---
+++ MVVMTest/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MVVMTest
+{
+    public class ActiveContentToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return System.Windows.Visibility.Hidden;
+
+            if (value.GetType().ToString() == "System.Windows.Forms.Integration.WindowsFormsHost")
+                return System.Windows.Visibility.Visible;
+            else
+                return System.Windows.Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new ArgumentException("Can't convert from Visibility to this");
+        }
+    }
+}

b42a158c69a8e1cccf79f135fd9a25635af8832b
cgz <chenguanzhou123@163.com>
2015-01-27 21:02:40 +0800

    fix bug

---
+++ MVVMTest/ViewModels/LayerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ESRI.ArcGIS.Controls;
+using MicroMvvm;
+using ESRI.ArcGIS.Carto;
+
+namespace MVVMTest.ViewModels
+{
+    public abstract class LayerViewModel : ObservableObject
+    {
+        #region LayerName;
+        abstract protected string layerName();
+        public string LayerName { get { return layerName(); } }
+        #endregion
+
+        #region DataLayer
+        protected ILayer dataLayer;
+        public ILayer DataLayer { get { return dataLayer; } }
+        #endregion       
+
+        #region IsValid
+        abstract protected bool isValid();
+        public bool IsValid { get { return isValid(); } }
+        #endregion
+    }
+}

0f267a79cd7a92753a24c304099d00c45e6230f7
sxq <yanhejin@163.com>
2015-04-13 20:34:19 +0800

    add delegatecommand

---
+++ GUI/MVVMBase/DelegateCommand.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GUI.MVVMBase
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public DelegateCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+
+            return _canExecute((parameter == null) ? default(T) : (T)Convert.ChangeType(parameter, typeof(T)));
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((parameter == null) ? default(T) : (T)Convert.ChangeType(parameter, typeof(T)));
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, EventArgs.Empty);
+        }
+    }
+    public class DelegateCommand : ICommand
+    {
+        /// <summary>
+        /// 命令所需执行的事件
+        /// </summary>
+        public Action<object> ExecuteCommand { get; set; }
+        /// <summary>
+        /// 命令是否可用所执行的事件
+        /// </summary>
+        public Func<object, bool> CanExecuteCommand { get; set; }
+
+        public DelegateCommand()
+        {
+        }
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canexecute)
+        {
+            ExecuteCommand = execute;
+            CanExecuteCommand = canexecute;
+        }
+
+        /// <summary>
+        /// 命令可用性获取
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteCommand(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// 命令具体执行
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            ExecuteCommand(parameter);
+        }
+    }
+ 
+}
